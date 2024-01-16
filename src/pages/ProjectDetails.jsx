6518f6694381409fddcbdb967686d303c6b7cace
Rita Martins <mazedoodle@gmail.com>
2024-01-16 12:58:11 +0000

    fixed merge confllicts

--- src/pages/ProjectDetails.jsx
+++ src/pages/ProjectDetails.jsx
@@ -26,6 +26,9 @@ import {
 function ProjectDetails() {
   const [project, setProject] = useState(null);
   const [edited, setEdited] = useState(true);
+  const [interested, setInterested] = useState(null);
+  const [matches, setMatches] = useState([]);
+  const [foundMatchId, setFoundMatchId] = useState(null);
 
   const { projectId, userType } = useParams();
   const navigate = useNavigate();
@@ -40,6 +43,60 @@ function ProjectDetails() {
     } catch (error) {
       console.log(error);
     }
+  };
+
+  const displayProject = () => {
+    return (
+      <>
+        <Flex justifyContent="space-between" mb={12}>
+          <Heading as="h1" size="xl">
+            {project.challengeName}
+          </Heading>
+          <HStack spacing={6}>
+            <Button colorScheme="red" onClick={onOpen} variant="link">
+              Delete Project
+            </Button>
+            <EditProjectDetails edited={edited} setEdited={setEdited} />
+          </HStack>
+        </Flex>
+        <Grid templateColumns="repeat(6, 1fr)" gap={4}>
+          <GridItem colSpan={4}>
+            <VStack align="left">
+              <Heading as="h2" size="md">
+                About the challenge:
+              </Heading>
+              <p>{project.challengeDescription}</p>
+              <AspectRatio maxW="560px" ratio={1}>
+                <iframe
+                  title={`${project.challengeName}'s video`}
+                  src={project.videoSubmission}
+                  allowFullScreen
+                />
+              </AspectRatio>
+            </VStack>
+          </GridItem>
+          <GridItem colSpan={2}>
+            {project.stakeholders.map(stakeholder => {
+              return (
+                <div key={project.id} className="stakeholder-info">
+                  <VStack align="left">
+                    <Heading as="h2" size="md">
+                      Stakeholder information:
+                    </Heading>
+                    <p>
+                      {stakeholder.name} -{' '}
+                      <Link href={`mailto:${stakeholder.email}`} isExternal>
+                        {stakeholder.email}
+                      </Link>
+                    </p>
+                  </VStack>
+                </div>
+              );
+            })}
+          </GridItem>
+        </Grid>
+      </>
+    );
   };
 
   const deleteProject = async () => {
@@ -58,6 +115,64 @@ function ProjectDetails() {
     getSingleProject();
   }, [projectId, edited]);
 
+  // NEEDS REVIEW
+  const getMatchId = async () => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_API_URL}/projectsStudents`
+      );
+
+      setMatches(response.data);
+
+      const foundMatch = matches.find(
+        match => match.projectId === +projectId && match.studentId === +userType
+      );
+
+      setFoundMatchId(foundMatch.id);
+      setInterested(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // useEffect(() => {
+  //   getMatchId();
+  // }, [interested]);
+
+  const handleInterest = async () => {
+    if (interested === false) {
+      try {
+        const projectsStudents = {
+          projectId: projectId,
+          studentId: userType,
+        };
+
+        const requestBody = { projectsStudents };
+
+        await axios.post(
+          `${import.meta.env.VITE_API_URL}/projectsStudents`,
+          requestBody
+        );
+
+        setInterested(!interested);
+        console.log(interested);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      try {
+        await axios.delete(
+          `${import.meta.env.VITE_API_URL}/projectsStudents/${foundMatchId}`
+        );
+
+        setInterested(!interested);
+        console.log(interested);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
   // alert to delete
   const { isOpen, onOpen, onClose } = useDisclosure();
   const cancelRef = useRef();
@@ -66,7 +181,10 @@ function ProjectDetails() {
     <div className="ProjectDetails">
       <Box p="120px 80px 32px">
         {+userType > 500 && (
-          <Button onClick={handleInterest}>Show Interest</Button>
+          <>
+            <Button onClick={handleInterest}>Show Interest</Button>
+            {project && displayProject()}
+          </>
         )}
         {+userType < 100 && (
           <>
@@ -105,56 +223,7 @@ function ProjectDetails() {
                     </AlertDialogContent>
                   </AlertDialogOverlay>
                 </AlertDialog>
-                <Flex justifyContent="space-between" mb={12}>
-                  <Heading as="h1" size="xl">
-                    {project.challengeName}
-                  </Heading>
-                  <HStack spacing={6}>
-                    <Button colorScheme="red" onClick={onOpen} variant="link">
-                      Delete Project
-                    </Button>
-                    <EditProjectDetails edited={edited} setEdited={setEdited} />
-                  </HStack>
-                </Flex>
-                <Grid templateColumns="repeat(6, 1fr)" gap={4}>
-                  <GridItem colSpan={4}>
-                    <VStack align="left">
-                      <Heading as="h2" size="md">
-                        About the challenge:
-                      </Heading>
-                      <p>{project.challengeDescription}</p>
-                      <AspectRatio maxW="560px" ratio={1}>
-                        <iframe
-                          title={`${project.challengeName}'s video`}
-                          src={project.videoSubmission}
-                          allowFullScreen
-                        />
-                      </AspectRatio>
-                    </VStack>
-                  </GridItem>
-                  <GridItem colSpan={2}>
-                    {project.stakeholders.map(stakeholder => {
-                      return (
-                        <div key={project.id} className="stakeholder-info">
-                          <VStack align="left">
-                            <Heading as="h2" size="md">
-                              Stakeholder information:
-                            </Heading>
-                            <p>
-                              {stakeholder.name} -{' '}
-                              <Link
-                                href={`mailto:${stakeholder.email}`}
-                                isExternal
-                              >
-                                {stakeholder.email}
-                              </Link>
-                            </p>
-                          </VStack>
-                        </div>
-                      );
-                    })}
-                  </GridItem>
-                </Grid>
+                {displayProject()}
               </>
             )}
           </>
